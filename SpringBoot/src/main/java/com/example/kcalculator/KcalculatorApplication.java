5160896aac146b730a4cc4f39d829bb02e2b23f0
getupminaaa <getupmina0405@gmail.com>
2021-06-11 10:48:35 +0900

    spring boot project 생성

---
+++ SpringBoot/src/main/java/com/example/kcalculator/KcalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.kcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KcalculatorApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/src/test/java/com/example/kcalculator/KcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
