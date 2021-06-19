6a59927edec73f8b2b3fa42a57e10a0c9fa7de2f
getupminaaa <getupmina0405@gmail.com>
2021-06-19 15:12:40 +0900

    글쓰기 구현 중

---
+++ AOS/app/src/main/java/com/example/kcalculator/view/CreateRecordActivity.kt
@@ -0,0 +1,14 @@
+package com.example.kcalculator.view
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kcalculator.R
+
+class CreateRecordActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_record)
+
+    }
+}
